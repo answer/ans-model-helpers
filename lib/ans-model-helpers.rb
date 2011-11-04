d7ff9474a695bd1004601a6ef9c0eea7e53fa2ab
sakai shunsuke <sakai@ans-web.co.jp>
2011-11-04 12:10:29 +0900

    sql_spec_helper の追加

--- lib/ans-model-helpers.rb
+++ lib/ans-model-helpers.rb
@@ -4,6 +4,7 @@ module Ans
   module Model
     module Helpers
       autoload :DateSumupHelper, "ans-model-helpers/date_sumup_helper"
+      autoload :SqlSpecHelper,   "ans-model-helpers/sql_spec_helper"
     end
   end
 end
