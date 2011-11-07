c8235a10900f3771758715152742801fa5f1fd9b
sakai shunsuke <sakai@ans-web.co.jp>
2011-11-07 20:16:16 +0900

    association のスペックヘルパーを追加

--- lib/ans-model-helpers/sql_spec_helper.rb
+++ lib/ans-model-helpers/sql_spec_helper.rb
@@ -8,6 +8,10 @@ module Ans::Model::Helpers
     def scope_の引数は(*params)
       @scope_params = params
     end
+
+    alias_method :"method_は", :"scope_は"
+    alias_method :"method_の引数は", :"scope_の引数は"
+
     def sql_は
       @sql = proc{|params|
         params ||= {}
@@ -19,5 +23,31 @@ module Ans::Model::Helpers
     def sql_の引数は(params)
       @sql_params = params
     end
+
+    def has_many_sql
+      proc{|sql|
+        sql <<  "SELECT"
+        sql <<   " `#{@scope_name}`.*"
+        sql << " FROM"
+        sql <<   " `#{@scope_name}`"
+        sql << " WHERE"
+        sql <<   " (`#{@scope_name}`.#{model.to_s.underscore}_id = #{@item.id})"
+      }
+    end
+
+    def has_many_through_sql(through)
+      proc{|sql|
+        sql <<  "SELECT"
+        sql <<   " `#{@scope_name}`.*"
+        sql << " FROM"
+        sql <<   " `#{@scope_name}`"
+        sql << " INNER JOIN"
+        sql <<   " `#{through}`"
+        sql <<   " ON `#{@scope_name}`.id = `#{through}`.#{@scope_name.to_s.singularize}_id"
+        sql << " WHERE"
+        sql <<   " ((`#{through}`.#{model.to_s.underscore}_id = #{@item.id}))"
+      }
+    end
+
   end
 end
